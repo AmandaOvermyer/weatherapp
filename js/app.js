45546a9b9bc85ce0abf3ba05eabca2c9860ffafa
Amanda Overmyer <aovermyerphoto@gmail.com>
2016-08-28 19:48:34 -0400

    added buttons to go through all three pages

--- js/app.js
+++ js/app.js
@@ -1,9 +1,32 @@
 $(document).ready(function(){
 
 	$(".js-login").click(function(){
-		$(".splash-screen").hide();
-		$(".calendar-selection").show();
+		gapi.auth2.getAuthInstance().signIn().then(function(){
+			$(".splash-screen").hide();
+			$(".calendar-selection").show();
+			makeApiCall();
+		})
+		
 	})
+
+	function makeApiCall() {
+		var restRequest = gapi.client.request({
+			'path': '/calendar/v3/users/me/calendarList',
+		}).then(function(resp) {
+			for (var i=0; i < resp.result.items.length; i++) {
+				var calendar = resp.result.items[i];
+				$('.calendar-choices').append("<p><a class='calendar-link' href='#'>" +  calendar.summary + "</a></p>");
+			}
+		}, function(reason) {
+			console.log('Error: ' + reason.result.error.message);
+		})
+	}
+
+	$(".calendar-choices").on("click", ".calendar-link", function(){
+		$(".calendar-selection").hide();
+		$(".calendar-view").show();
+	})
+
 
 	$(".js-logout").click(function(){
 		$(".calendar-selection").hide();
@@ -14,5 +37,22 @@ $(document).ready(function(){
 		$(".calendar-selection").hide();
 		$(".calendar-view").show();
 	})
+
+	$(".js-changecalendar").click(function(){
+		$(".calendar-view").hide();
+		$(".calendar-selection").show();
+	})
+
+	function onLoadFn(){
+		gapi.client.setApiKey('AIzaSyCX5Zh7ZtlaU2mvksDKQe5Z_njZ-zc7Mdo');
+  gapi.auth2.init({
+      client_id: '963904802029-arblg7v5te79ao18cqjc6006mr6scsmo.apps.googleusercontent.com',
+      scope: 'https://www.googleapis.com/auth/calendar.readonly'
+  });
+	}
+
+	gapi.load("client:auth2", onLoadFn);
+
+
 }) 
 
